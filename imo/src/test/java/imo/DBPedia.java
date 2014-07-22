b4b8d957eff27eaac9a00de4696a2c006a9d153f
tnas <nascimenthiago@gmail.com>
2014-07-22 14:10:37 -0300

    Alterações nos botões da tela principal.

--- imo/src/test/java/imo/DBPedia.java
+++ imo/src/test/java/imo/DBPedia.java
@@ -2,12 +2,8 @@ package imo;
 
 import java.io.IOException;
 import java.net.Authenticator;
-import java.net.URI;
 import java.net.URISyntaxException;
-
-import org.apache.jena.atlas.web.auth.ApacheModAuthFormLogin;
-import org.apache.jena.atlas.web.auth.FormLogin;
-import org.apache.jena.atlas.web.auth.FormsAuthenticator;
+import java.util.Properties;
 
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
@@ -17,7 +13,6 @@ import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.Literal;
-import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
 
 public class DBPedia {
 
@@ -26,26 +21,25 @@ public class DBPedia {
 		final String userProxy = "thiago.rodrigues";
 		final String passwordProxy = "$t1n8r97$";
 		
-		Authenticator.setDefault(new ProxyAuthenticator(userProxy, passwordProxy));
+		/*Authenticator.setDefault(new ProxyAuthenticator(userProxy, passwordProxy));
 		System.setProperty("http.proxySet", "true");
 		System.setProperty("http.proxyHost", "proxy.tre-es.gov.br");
 		System.setProperty("http.proxyPort", "3128");
 		System.setProperty("http.proxyUser", userProxy);
-		System.setProperty("http.proxyPassword", passwordProxy);
+		System.setProperty("http.proxyPassword", passwordProxy);*/
 		
+		System.setProperty("java.net.useSystemProxies", "true");
+		System.setProperty("socksProxyHost", "proxy.tre-es.gov.br");
+		System.setProperty("socksProxyPort", "3128");
+		/*System.setProperty("java.net.socks.username", userProxy);
+		System.setProperty("java.net.socks.password", passwordProxy);*/
 		
-/*		 final java.net.URL url = new java.net.URL( "http://www.google.com" );
-	     final java.net.URLConnection uconn = url.openConnection( );
-	     final java.net.HttpURLConnection conn = (java.net.HttpURLConnection)uconn;
-	 
-	        // Set up a request.
-	        conn.setConnectTimeout( 10000 );    // 10 sec
-	        conn.setReadTimeout( 10000 );       // 10 sec
-	        conn.setInstanceFollowRedirects( true );
-	        conn.setRequestProperty( "User-agent", "spider" );
-	 
-	        // Send the request.
-	        conn.connect( );*/
+		Authenticator.setDefault(new ProxyAuthenticator(userProxy, passwordProxy));
+
+		Properties prop = System.getProperties();
+		
+		for (Object key : prop.keySet())
+			System.out.println(key + " : " + prop.getProperty((String) key));
 		
 		
 		
@@ -64,12 +58,8 @@ public class DBPedia {
 		+ " FILTER ( lang(?artistName) = \"en\")"
 		+ " FILTER ( lang(?albumName) = \"en\" )" + " }";
 	
-		URI targetService = new URI("http://dbpedia.org/sparql");
-		FormLogin formLogin = new ApacheModAuthFormLogin("proxy.tre-es.gov.br", userProxy, passwordProxy.toCharArray());
-		FormsAuthenticator authenticator = new FormsAuthenticator(targetService, formLogin);
-		
 		Query query = QueryFactory.create(query1);
-		QueryExecution queryExecution = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query, authenticator);
+		QueryExecution queryExecution = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
 		
 		ResultSet results = queryExecution.execSelect();
 		ResultSetFormatter.out(System.out, results);
