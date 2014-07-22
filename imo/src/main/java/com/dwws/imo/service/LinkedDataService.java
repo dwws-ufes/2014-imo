e2c40dba7af31abc1a5ebf1b7a57e43bd4d73da2
tnas <nascimenthiago@gmail.com>
2014-07-22 18:00:49 -0300

    Linked data service

---
+++ imo/src/main/java/com/dwws/imo/service/LinkedDataService.java
@@ -0,0 +1,12 @@
+package com.dwws.imo.service;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class LinkedDataService {
+	
+	public String buscarDescricaoCargo(String nomeCargo) {
+		return "Linked Data --- Descrição Cargo";
+	}
+
+}
